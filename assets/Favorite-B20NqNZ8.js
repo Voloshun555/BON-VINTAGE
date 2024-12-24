f3974086f2e2deb2f65c24c5a5fc2fbfad497fbd
Voloshun555 <94966717+Voloshun555@users.noreply.github.com>
2024-12-24 16:54:33 +0000

    Deploying to gh-pages from @ Voloshun555/BON-VINTAGE@1db4cf02fdf4b3fe4122664eb214e3811eedde5c 🚀

--- assets/Favorite-B20NqNZ8.js
+++
@@ -1,1 +0,0 @@
-import{u as n,j as r}from"./index-pbYxM8yr.js";import{u as c,C as d}from"./CardList-B-coHBdT.js";import{s as f}from"./Home.module-C8ooPR02.js";const p=()=>{const{data:s=[],isLoading:o,isError:a}=c(12),i=n(t=>t.favorite),e=s.filter(t=>i.includes(t.id));return r.jsx("section",{className:f.containerFavorite,children:r.jsx(d,{data:e,isLoading:o,isError:a})})};export{p as default};
