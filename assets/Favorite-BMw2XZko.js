67e684dce1f86c05f450fc7911d189f5122b786f
Voloshun555 <94966717+Voloshun555@users.noreply.github.com>
2024-12-25 11:27:55 +0000

    Deploying to gh-pages from @ Voloshun555/BON-VINTAGE@54fb4f5e1421f5c56125945b6b56c51c5cdfa73e 🚀

--- assets/Favorite-BMw2XZko.js
+++
@@ -1,1 +0,0 @@
-import{u as n,j as r}from"./index-De9o_7Zx.js";import{u as c,C as d}from"./CardList-Cqj-uhTc.js";import{s as f}from"./Home.module-C8ooPR02.js";const p=()=>{const{data:s=[],isLoading:o,isError:a}=c(12),i=n(t=>t.favorite),e=s.filter(t=>i.includes(t.id));return r.jsx("section",{className:f.containerFavorite,children:r.jsx(d,{data:e,isLoading:o,isError:a})})};export{p as default};
---
+++ assets/Favorite-mAUPcH2x.js
@@ -0,0 +1,1 @@
+import{u as n,j as r}from"./index-CDRoLlX_.js";import{u as c,C as d}from"./CardList-xuJgNFR-.js";import{s as f}from"./Home.module-C8ooPR02.js";const p=()=>{const{data:s=[],isLoading:o,isError:a}=c(12),i=n(t=>t.favorite),e=s.filter(t=>i.includes(t.id));return r.jsx("section",{className:f.containerFavorite,children:r.jsx(d,{data:e,isLoading:o,isError:a})})};export{p as default};
