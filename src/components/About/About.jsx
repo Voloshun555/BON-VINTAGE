9abf7485d9d5c31c728fd077146e93012ce854cf
Voloshun <94966717+Voloshun555@users.noreply.github.com>
2025-06-20 21:18:37 +0300

    add about mission

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,38 @@
-import { Title } from '../Title/Title'
-// import s from './About.module.scss'
+import { Title } from "../Title/Title";
+import s from "./About.module.scss";
 export const About = () => {
   return (
-    <div>
-        <Title/>
-    </div>
-  )
-}
+    <>
+      <section>
+        <Title />
+      </section>
+      <section className={s.ourMission}>
+        <div className={s.containerMission}>
+          <ul className={s.listMission}>
+            <li className={s.elemMission}>
+              <div className={s.iconMission}>Тут буде картинка</div>
+            </li>
+            <li className={s.elemMission}>
+              <h2 className={s.title}>our mission</h2>
+              <p className={s.textMission}>
+                Наша місія – приносити у ваш дім натхнення наповнити ваш простір
+                історією, зберігати історію та ділитися її красою з вами
+              </p>
+            </li>
+            <li className={s.elemMission}>
+              <h2 className={s.title}>our goal</h2>
+              <p className={s.textMission}>
+                Наша мета – додати вашому простору індивідуальності через
+                історичні речі. Допомогти вам знайти щось особливе, що змінить
+                ваш дім.
+              </p>
+            </li>
+            <li className={s.elemMission}>
+              <div className={s.iconMission}>Тут буде картинка</div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+};
