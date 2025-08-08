52604627eee1255f4c04f7b22dc08618aecae76f
Voloshun555 <94966717+Voloshun555@users.noreply.github.com>
2025-08-08 09:52:57 +0000

    Deploying to gh-pages from @ Voloshun555/BON-VINTAGE@5b3571ab46131924e3931a2dc88d8a60e1656227 🚀

---
+++ assets/Title-DhhCriTk.js
@@ -0,0 +1,5 @@
+import{j as t,I as l,x as e}from"./index-CdC8j1xr.js";const h="_container_tk4bd_31",m="_iconWithStar_tk4bd_37",x="_title_tk4bd_41",_="_historyItems_tk4bd_55",s={container:h,iconWithStar:m,title:x,historyItems:_},j=({icon:r=!0,title:c=`Antique & Vintage 
+Exclusive Collectibles`,subtitle:a=`Предмети з історією, які наповнюють ваш дім 
+унікальністю та чарівністю`,className:o=""})=>t.jsxs("div",{className:`${s.container} ${o}`,children:[r&&t.jsx(l,{id:"icon-with-star",className:s.iconWithStar}),t.jsx("h1",{className:s.title,children:c.split(`
+`).map((i,n)=>t.jsxs(e.Fragment,{children:[i,t.jsx("br",{})]},n))}),t.jsx("p",{className:s.historyItems,children:a.split(`
+`).map((i,n)=>t.jsxs(e.Fragment,{children:[i,t.jsx("br",{})]},n))})]});export{j as T};
