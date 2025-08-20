992e2ac9c3981364fc460786da72e3d8369b0cd5
Voloshun555 <94966717+Voloshun555@users.noreply.github.com>
2025-08-20 12:31:59 +0000

    Deploying to gh-pages from @ Voloshun555/BON-VINTAGE@d143d4d537913e54ff36523ee041b3707c52fff7 🚀

--- assets/Title-Bsr9SRyy.js
+++ assets/Title-Bsr9SRyy.js
@@ -1,4 +1,4 @@
-import{j as t,I as l,R as e}from"./index-48HVN_Wr.js";const h="_container_157r7_31",m="_iconWithStar_157r7_37",x="_title_157r7_40",_="_historyItems_157r7_54",s={container:h,iconWithStar:m,title:x,historyItems:_},p=({icon:r=!0,title:c=`Antique & Vintage 
+import{j as t,I as l,R as e}from"./index-DQHOuro1.js";const h="_container_157r7_31",m="_iconWithStar_157r7_37",x="_title_157r7_40",_="_historyItems_157r7_54",s={container:h,iconWithStar:m,title:x,historyItems:_},p=({icon:r=!0,title:c=`Antique & Vintage 
 Exclusive Collectibles`,subtitle:a=`Предмети з історією, які наповнюють ваш дім 
 унікальністю та чарівністю`,className:o=""})=>t.jsxs("div",{className:`${s.container} ${o}`,children:[r&&t.jsx(l,{id:"icon-with-star",className:s.iconWithStar}),t.jsx("h1",{className:s.title,children:c.split(`
 `).map((i,n)=>t.jsxs(e.Fragment,{children:[i,t.jsx("br",{})]},n))}),t.jsx("p",{className:s.historyItems,children:a.split(`
