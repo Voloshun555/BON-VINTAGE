d5b2ea828993bffe9487f6c9a2817282934b48b2
Voloshun555 <94966717+Voloshun555@users.noreply.github.com>
2025-08-21 12:21:23 +0000

    Deploying to gh-pages from @ Voloshun555/BON-VINTAGE@1af825f90a85d3b8aff1cc4a6ffd16c3517be045 🚀

--- assets/Title-BETQ2STl.js
+++
@@ -1,5 +0,0 @@
-import{j as t,I as l,R as e}from"./index-BRXwTAdE.js";const m="_container_1m3zk_31",h="_iconWithStar_1m3zk_37",x="_title_1m3zk_40",_="_historyItems_1m3zk_54",s={container:m,iconWithStar:h,title:x,historyItems:_},p=({icon:r=!0,title:c=`Antique & Vintage 
-Exclusive Collectibles`,subtitle:a=`Предмети з історією, які наповнюють ваш дім 
-унікальністю та чарівністю`,className:o=""})=>t.jsxs("div",{className:`${s.container} ${o}`,children:[r&&t.jsx(l,{id:"icon-with-star",className:s.iconWithStar}),t.jsx("h1",{className:s.title,children:c.split(`
-`).map((i,n)=>t.jsxs(e.Fragment,{children:[i,t.jsx("br",{})]},n))}),t.jsx("p",{className:s.historyItems,children:a.split(`
-`).map((i,n)=>t.jsxs(e.Fragment,{children:[i,t.jsx("br",{})]},n))})]});export{p as T};
---
+++ assets/Title-Dd79O-1m.js
@@ -0,0 +1,5 @@
+import{j as t,I as l,R as e}from"./index-CDPi_8L4.js";const h="_container_16uob_31",m="_iconWithStar_16uob_37",x="_title_16uob_40",_="_historyItems_16uob_54",s={container:h,iconWithStar:m,title:x,historyItems:_},u=({icon:o=!0,title:r=`Antique & Vintage 
+Exclusive Collectibles`,subtitle:c=`Предмети з історією, які наповнюють ваш дім 
+унікальністю та чарівністю`,className:a=""})=>t.jsxs("div",{className:`${s.container} ${a}`,children:[o&&t.jsx(l,{id:"icon-with-star",className:s.iconWithStar}),t.jsx("h1",{className:s.title,children:r.split(`
+`).map((i,n)=>t.jsxs(e.Fragment,{children:[i,t.jsx("br",{})]},n))}),t.jsx("p",{className:s.historyItems,children:c.split(`
+`).map((i,n)=>t.jsxs(e.Fragment,{children:[i,t.jsx("br",{})]},n))})]});export{u as T};
